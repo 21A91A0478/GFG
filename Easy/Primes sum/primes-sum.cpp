a2b9fee5e08cc567b8625599d89d975a4eb8fc92
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-01-16 11:13:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Primes sum/primes-sum.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+public:
+
+    int fun(int n){
+        if(n<=1)return 0;
+        for(int i = 2; i <= sqrt(n); i++){
+            if(n%i==0){
+                return 0;
+            }
+        }
+        return 1;
+    }
+    
+    string isSumOfTwo(int N){
+        // code here
+        for(int i = 2; i <= (N/2); i++){
+            if(fun(i) && fun(N-i)){
+                return "Yes";
+            }
+        }
+        return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        cout << ob.isSumOfTwo(N) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
