bacc7f671f1ff49bcd85afa907a66e994222b7d6
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-01-22 18:00:01 +0530

    Updated Solution - GfG to GitHub

--- Easy/Detect Loop in linked list/detect-loop-in-linked-list.cpp
+++ Easy/Detect Loop in linked list/detect-loop-in-linked-list.cpp
@@ -49,17 +49,17 @@ class Solution
     bool detectLoop(Node* head)
     {
         // your code here
-        unordered_map<Node*, int> mpp;
-        Node* temp = head;
-        while(temp){
-            if(mpp.find(temp)!=mpp.end()){
-                return true;
+        Node* slow=head;
+        Node* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
             }
-            mpp[temp] = 1;
-            temp = temp->next;
         }
-         
-        return false; 
+            return false;
     }
 };
 
