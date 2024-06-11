2deee4f1862ce27fb9616e8317a55bb405012a8a
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-06-11 15:53:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Print adjacency list/print-adjacency-list.cpp
+++ Easy/Print adjacency list/print-adjacency-list.cpp
@@ -9,7 +9,8 @@ class Solution {
     vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
         // Code here
         vector<vector<int>> adj(V);
-        for(int i = 0; i < edges.size(); i++){
+        for(int i = 0; i < edges.size(); i++)
+        {
             adj[edges[i].first].push_back(edges[i].second);
             adj[edges[i].second].push_back(edges[i].first);
         }
