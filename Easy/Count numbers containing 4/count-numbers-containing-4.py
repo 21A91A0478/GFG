3c18d83f0f4eac4419d8ed956a7624dc4b372ee4
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-06-12 21:27:04 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count numbers containing 4/count-numbers-containing-4.py
@@ -0,0 +1,26 @@
+
+class Solution:
+    def countNumberswith4(self, n : int) -> int:
+        # code here
+        c = 0
+        for i in range(1, n+1):
+            s = str(i)
+            if '4' in s:
+                c += 1
+        return c
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        obj = Solution()
+        res = obj.countNumberswith4(n)
+
+        print(res)
+
+# } Driver Code Ends
