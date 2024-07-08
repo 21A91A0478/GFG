a62b605cd9069ba0504ede5de2f1c1070f01633b
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-07-08 06:01:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Jump Game/jump-game.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+    def canReach(self, A, N):
+        # code here 
+        m = A[0]
+        for i in range(1,len(A)):
+            if m >= i:
+                m = max(m, A[i]+i)
+            else:
+                return 0
+            
+        return 1
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        A=list(map(int,input().split()))
+        
+        ob = Solution()
+        print(ob.canReach(A,N))
+# } Driver Code Ends
