9c0f17330c9b916932b835608168f596905c7d58
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-07-10 15:49:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate a Linked List/rotate-a-linked-list.py
@@ -0,0 +1,77 @@
+# Your task is to complete this function
+
+'''
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+'''
+
+
+class Solution:
+    
+    #Function to rotate a linked list.
+    def rotate(self, head, k):
+        
+        if not head or k == 0:
+            return head
+            
+        c = 1
+        temp = head
+        while temp.next:
+            temp = temp.next
+            c += 1
+        
+        temp.next = head
+        temp = head
+        for i in range(k-1):
+            temp = temp.next
+        ans = temp.next
+        temp.next = None
+        return ans
+
+
+
+#{ 
+ # Driver Code Starts
+# driver
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def insert(self,val):
+        if self.head is None:
+            self.head = Node(val)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(val)
+            self.tail = self.tail.next
+
+def printList(n):
+    while n:
+        print(n.data, end=' ')
+        n = n.next
+    print()
+
+if __name__=="__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [int(x) for x in input().split()]
+        k = int(input())
+        
+        lis = LinkedList()
+        for i in arr:
+            lis.insert(i)
+        
+        head = Solution().rotate(lis.head,k)
+        printList(head)
+# } Driver Code Ends
