ed765c5e57c593f8ffcc1048191cadf9002f55a9
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-09-13 13:45:55 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Length of the longest substring/length-of-the-longest-substring.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    def longestUniqueSubsttr(self, s):
+        # code here
+        n = len(s)
+        i = 0
+        j = 1
+        d = {}
+        d[s[i]] = i
+        m = 0
+        while i < j and j < n:
+            ele = s[j]
+            if ele not in d:
+                d[ele] = j
+                j += 1
+            else:
+                if d[ele] < i:
+                    d[ele] = j
+                    j += 1
+                else:
+                    k = j-i
+                    m = max(m, k)
+                    i = d[ele]+1
+                    d[ele] = j
+                    j += 1
+        m = max(m, j-i)
+        return m
+        
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        s = input().strip()
+        
+        
+        ob=Solution()
+        print(ob.longestUniqueSubsttr(s))
+# } Driver Code Ends
