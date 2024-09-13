99c86902ca98bf6f61e8d32d5111ab1af6662b50
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-09-13 14:12:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest Distinct characters in string/longest-distinct-characters-in-string.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+class Solution:
+
+    def longestSubstrDistinctChars(self, s):
+        # code here
+        n = len(s)
+        i = 0
+        j = 1
+        d = {}
+        d[s[i]] = i
+        m = 0
+        while i < j and j < n:
+            ele = s[j]
+            if ele not in d:
+                d[ele] = j
+                j += 1
+            else:
+                if d[ele] < i:
+                    d[ele] = j
+                    j += 1
+                else:
+                    k = j-i
+                    m = max(m, k)
+                    i = d[ele]+1
+                    d[ele] = j
+                    j += 1
+        m = max(m, j-i)
+        return m
+    
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        S = input()
+
+        solObj = Solution()
+
+        ans = solObj.longestSubstrDistinctChars(S)
+
+        print(ans)
+
+# } Driver Code Ends
