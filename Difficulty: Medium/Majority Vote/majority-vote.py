2d2bda39bbaa427bf4142d8f14f6355ebcc321e8
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-10-08 18:52:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Vote/majority-vote.py
@@ -0,0 +1,42 @@
+class Solution:
+    # Function to find the majority elements in the array
+    def findMajority(self, arr):
+        #Your Code goes here.
+        l = []
+        k = len(arr)//3
+        d = {}
+        for i in arr:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+                
+        for key in d.keys():
+            if d[key] > k:
+                l.append(key)
+        l.sort()
+        if l:
+            return l
+        else:
+            return [-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    t = int(input().strip())
+    for _ in range(t):
+        s = input().strip()
+        nums = list(map(int, s.split()))
+        ob = Solution()
+        ans = ob.findMajority(nums)
+        print(" ".join(map(str, ans)))
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
