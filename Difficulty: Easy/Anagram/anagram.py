00b1d301b9dd086f1115692ebc03ed6bd64584bf
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2024-11-30 17:59:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.py
@@ -0,0 +1,31 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function is to check whether two strings are anagram of each other or not.
+    def areAnagrams(self, s1, s2):
+        #code here
+        l1 = list(s1)
+        l2 = list(s2)
+        l1.sort()
+        l2.sort()
+        return l1 == l2
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        a = input().strip()
+        b = input().strip()
+        if (Solution().areAnagrams(a, b)):
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
