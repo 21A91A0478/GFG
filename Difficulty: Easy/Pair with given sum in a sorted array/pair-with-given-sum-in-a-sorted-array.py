6899a834f1b9048469ba6da508d2d9713abff0b7
Amrutha K <99873476+21A91A0478@users.noreply.github.com>
2025-01-07 17:26:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.py
@@ -0,0 +1,54 @@
+#User function Template for python3
+
+
+class Solution:
+    def countPairs (self, arr, target) : 
+        #Complete the function
+        d = {}
+        for i in arr:
+            if i not in d:
+                d[i] = 1
+            else:
+                d[i] += 1
+            
+        c = 0
+        for i in arr:
+            if i in d:
+                d[i] -= 1
+            
+            if target-i in d:
+                if d[target-i] > 0:
+                    c += d[target-i]
+            
+        return c
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().split('\n')
+
+    t = int(data[0].strip())
+    index = 1
+
+    for _ in range(t):
+        arr = list(map(int, data[index].strip().split()))
+        index += 1
+        target = int(data[index].strip())
+        index += 1
+
+        res = Solution().countPairs(arr, target)
+        print(res)
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
